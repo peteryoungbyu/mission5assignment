338a20c53e1433fea9967d206b69129f47da2098
peter <peter@LAPTOP17>
2026-02-04 09:12:37 -0700

    Add project files.

---
+++ mission5assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mission5assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
